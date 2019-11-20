54a6ea49521f8a79cf6eacc4160d3ad26f230812
Eric Dingle <eric.dingle@gmail.com>
2019-11-20 00:38:43 -0500

    Move ghosting update script and update ghosting.

---
+++ ghosting/update_ghosting.sh
@@ -0,0 +1,2 @@
+wget -O index.html https://raw.githubusercontent.com/ericdingle/glencoe/master/index.html
+wget -O index.js https://raw.githubusercontent.com/ericdingle/glencoe/master/index.js
--- tools/update_ghosting.sh
+++
@@ -1,1 +0,0 @@
-wget -O ghosting/index.html https://raw.githubusercontent.com/ericdingle/glencoe/master/index.html
